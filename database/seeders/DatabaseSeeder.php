0f0bfedec3a7a9f5cd9cb71f0cdeb0f9666b39af
kobayashi.s <sorappirnn33@gmail.com>
2026-09-01 22:07:16 +0900

    課題修正完了：DatabaseSeederおよびリレーション設定の追加

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,19 +17,19 @@ class DatabaseSeeder extends Seeder
     {
         // ★ テスト用のメーカー（会社）データを登録します
         Company::create([
-            'company_name' => 'テストメーカーA',
+            'company_name' => 'コカ・コーラ',
             'street_address' => '東京都千代田区1-1',
             'representative_name' => '山田太郎',
         ]);
 
         Company::create([
-            'company_name' => 'テストメーカーB',
+            'company_name' => 'サントリー',
             'street_address' => '大阪府大阪市1-1',
             'representative_name' => '佐藤花子',
         ]);
 
         Company::create([
-            'company_name' => 'テストメーカーC',
+            'company_name' => '伊右衛門',
             'street_address' => '愛知県名古屋市1-1',
             'representative_name' => '鈴木一郎',
         ]);
